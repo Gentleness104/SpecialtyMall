47ca51083828cb005e9759399b89c5abaf0d99a5
changbudadehaiz <97407563@qq.com>
2018-07-25 10:40:13 +0800

    首次提交

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import DocumentTitle from 'react-document-title';
+
+import {
+  Route,
+} from 'react-router-dom'
+
+import { Button } from 'antd-mobile';
+
+class App extends Component {
+  render() {
+    return (
+      <DocumentTitle title='HomeTitle'>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <Button>Click</Button>
+        </div>
+      </DocumentTitle>
+    );
+  }
+}
+
+export default App;
